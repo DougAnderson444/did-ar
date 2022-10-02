3c274bf076353d6dbc16720f3e0cceb2d576cc81
Doug Anderson <douganderson444@gmail.com>
2022-10-02 18:49:32 -0300

    0.0.1

--- src/lib/DIDAr.svelte
+++ src/lib/DIDAr.svelte
@@ -1,17 +1,25 @@
-<script>
+<script lang='ts'>
 	import { onMount } from 'svelte';
-	import { DIDAr } from 'didar';
-	import { DIDArConfig } from './DIDArConfig.js';
+	import { DIDAr } from './didar';
+	import { DIDArConfig } from './DIDArConfig';
 
-	export let config = DIDArConfig;
-	export let didar = null;
+	export let config = {dev: true} 
+	export let didar: DIDAr
 
 	onMount(async () => {
 		didar = new DIDAr(config);
-		await didar.init();
+		// await didar.init();
 	});
+
+	async function handleCreateDID(){
+		// await didar.create()
+	}
 </script>
 
-DIDAr.svelte
+<div class='bg-blue-600 hover:bg-blue-500 shadow rounded-lg m-4 p-4 w-fit text-white cursor-pointer'
+on:click={handleCreateDID}
+>
+Create Identity
+</div>
 
 <style lang="postcss"></style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -13,7 +19,7 @@ const config = {
 					: {
 							// use @import when building packages for distribution
 							configFilePath: path.resolve(__dirname, './postcss.config.js'),
-							prependData: `@import '${path.resolve('./src/utilities.css')}';`
+							prependData: `@import '${path.resolve('./src/app.css')}';`
 					  }
 		})
 	],
