9e076d0c6b6302ff9ffd0349ce99e211f3ca377e
Doug A <douganderson444@gmail.com>
2022-10-04 16:21:17 -0300

    add wallet to routing demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,22 @@
-<script>
+<script lang="ts">
 	import { DIDAr } from '@peerpiper/did-ar';
-	// import { Web3WalletMenu } from "@peerpiper/web3-wallet-connector"
+	import { Web3WalletMenu } from '@peerpiper/web3-wallet-connector';
+	// @ts-ignore
+	import type { handlers } from '@peerpiper/iframe-wallet-sdk';
+
+	let wallet: handlers;
+	let RSAPublicKey: { kty: string; n: string; e: string; kid?: string };
+	let Ed25519PublicKey: Uint8Array;
+
+	const walletReady = async (e: CustomEvent) => {
+		wallet = e.detail.wallet;
+		RSAPublicKey = await wallet.arweaveWalletAPI.getActivePublicKey();
+		Ed25519PublicKey = await wallet.proxcryptor.getPublicKey();
+	};
 </script>
 
-<DIDAr />
+<Web3WalletMenu on:walletReady={walletReady} />
+
+{#if wallet && RSAPublicKey && Ed25519PublicKey}
+	<DIDAr {wallet} {RSAPublicKey} {Ed25519PublicKey} />
+{/if}
