dea8dc1482a64cd29d52bc93e49bb66a4bd3b9d6
Doug A <douganderson444@gmail.com>
2022-10-04 16:22:05 -0300

    add handleCreateDID

--- src/lib/DIDAr.svelte
+++ src/lib/DIDAr.svelte
@@ -1,25 +1,42 @@
-<script lang='ts'>
+<script lang="ts">
 	import { onMount } from 'svelte';
-	import { DIDArConfig } from './DIDArConfig';
+	// @ts-ignore
+	import type { handlers } from '@peerpiper/iframe-wallet-sdk';
 
-	export let config = {dev: true} 
-	export let didar: DIDAr
+	export let RSAPublicKey: { kty: string; n: string; e: string; kid?: string };
+	export let Ed25519PublicKey: Uint8Array;
+	export let wallet: handlers;
+
+	let createDid: Function;
+	let did: string;
+	// fund arlocal for this RSA key address
+	let walletAddress;
 
 	onMount(async () => {
-		const { DIDAr } = await import('./didar');
-		didar = new DIDAr(config);
-		// await didar.init();
+		({ createDid } = await import('./didar'));
 	});
 
-	async function handleCreateDID(){
-		// await didar.create()
+	async function handleCreateDID() {
+		walletAddress = await wallet.arweaveWalletAPI.getActiveAddress();
+
+		did = await createDid({
+			RSAPublicKey,
+			Ed25519PublicKey,
+			options: { walletAddress }
+		});
 	}
 </script>
 
-<div class='bg-blue-600 hover:bg-blue-500 shadow rounded-lg m-4 p-4 w-fit text-white cursor-pointer'
-on:click={handleCreateDID}
+<div
+	class="bg-blue-600 hover:bg-blue-500 shadow rounded-lg m-4 p-4 w-fit text-white cursor-pointer"
+	on:click={handleCreateDID}
 >
-Create Identity
+	Create Identity on {process.env.NODE_ENV}
 </div>
 
-<style lang="postcss"></style>
+{#if did}
+	<div class="bg-green-600 hover:bg-green-500 shadow rounded-lg m-4 p-4 w-fit text-white">
+		{did}
+	</div>
+{/if}
+<!-- <style lang="postcss"></style> -->
