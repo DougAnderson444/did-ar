a1b144edf952a65f19533283c1a5957981b53342
Doug A <douganderson444@gmail.com>
2022-10-04 18:26:24 -0300

    export updateDid

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,3 @@
 export { default as DIDAr } from './DIDAr.svelte';
-export { createDidAr } from './didar';
+export { createDidAr, updateDidDoc } from './didar';
 export * as didArResolver from './resolver';
