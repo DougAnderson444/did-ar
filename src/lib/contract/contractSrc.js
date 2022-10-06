988c12c20a733230930029f2cca1786fa995b89f
Doug A <douganderson444@gmail.com>
2022-10-06 09:34:45 -0300

    streamline the contract source

--- src/lib/contract/contractSrc.js
+++ src/lib/contract/contractSrc.js
@@ -7,7 +7,6 @@ export async function handle(state, action) {
 	}
 
 	if (input.function === 'update') {
-		// prevent people from accidentally updating their id
 		if (!state.id) {
 			if (!input.id.startsWith('did:ar')) {
 				throw new ContractError('Invalid ID');
@@ -15,22 +14,18 @@ export async function handle(state, action) {
 			state.id = input.id;
 		}
 
-		if (action.input.verificationMethod) {
-			// verify that action.input.verificationMethod is an array
-			if (
-				!Array.isArray(action.input.verificationMethod) ||
-				action.input.verificationMethod.length == 0
-			) {
+		if (input.verificationMethod) {
+			if (!Array.isArray(input.verificationMethod) || input.verificationMethod.length === 0) {
 				throw new ContractError('verificationMethod must be an array with at least one method');
 			}
-			state.verificationMethod = action.input.verificationMethod;
+			state.verificationMethod = input.verificationMethod;
 		}
-		state.authentication = action.input?.authentication || state.authentication;
-		state.assertionMethod = action.input?.assertionMethod || state.assertionMethod;
-		state.keyAgreement = action.input?.keyAgreement || state.keyAgreement;
-		state.capabilityInvocation = action.input?.capabilityInvocation || state.capabilityInvocation;
-		state.capabilityDelegation = action.input?.capabilityDelegation || state.capabilityDelegation;
-		state.service = action.input?.service || state.service;
+		state.authentication = input?.authentication || state.authentication;
+		state.assertionMethod = input?.assertionMethod || state.assertionMethod;
+		state.keyAgreement = input?.keyAgreement || state.keyAgreement;
+		state.capabilityInvocation = input?.capabilityInvocation || state.capabilityInvocation;
+		state.capabilityDelegation = input?.capabilityDelegation || state.capabilityDelegation;
+		state.service = input?.service || state.service;
 	}
 
 	return { state };
