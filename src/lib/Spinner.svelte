97351bdccf03e2039eb43a9570fa0bdab47d9214
Doug A <douganderson444@gmail.com>
2022-11-06 07:14:20 -0400

    simplify spinner

--- src/lib/Spinner.svelte
+++ src/lib/Spinner.svelte
@@ -1,26 +1,40 @@
-<script>
-</script>
-
-<div
-	class="inline-flex items-center px-4 py-2
-        font-semibold leading-6 text-sm shadow-md rounded-md
-        text-white
-        bg-green-500
-        hover:bg-green-400
-        transition ease-in-out duration-150 cursor-progress"
->
-	<svg
-		class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-		xmlns="http://www.w3.org/2000/svg"
-		fill="none"
-		viewBox="0 0 24 24"
-	>
-		<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
-		<path
-			class="opacity-75"
-			fill="currentColor"
-			d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-		/>
-	</svg>
-	<slot />
+<div class="flex flex-row items-center">
+	<div class="spinner" />
+	<div class="flex-1">
+		<slot />
+	</div>
 </div>
+
+<style>
+	.flex-row {
+		flex-direction: row;
+	}
+	.flex {
+		display: flex;
+	}
+	.flex-1 {
+		flex: 1 1 0%;
+	}
+	.items-center {
+		align-items: center;
+	}
+	.spinner {
+		aspect-ratio: 1;
+		height: 1em;
+		width: 1em;
+		border: 0.3em dotted rgba(250, 204, 21);
+		border-radius: 9999px;
+		animation: spin 1s linear infinite;
+		background: #fcff23;
+		margin: 0.25em;
+	}
+
+	@keyframes spin {
+		from {
+			transform: rotate(0deg);
+		}
+		to {
+			transform: rotate(360deg);
+		}
+	}
+</style>
